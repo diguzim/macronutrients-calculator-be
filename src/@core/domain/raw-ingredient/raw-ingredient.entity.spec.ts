0510163d37e2c977987dc2afbcb899729ce017de
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-09-27 17:11:31 -0300

    Creating new initialization method for raw ingredient that initializes it based on absolute values

--- src/@core/domain/raw-ingredient/raw-ingredient.entity.spec.ts
+++ src/@core/domain/raw-ingredient/raw-ingredient.entity.spec.ts
@@ -133,4 +133,133 @@ describe('RawIngredient', () => {
       ).not.toThrowError('Ratios do not sum up to one');
     });
   });
+
+  describe('createFromAbsoluteValues', () => {
+    it('should create a RawIngredient when values are valid', () => {
+      const rawIngredient = RawIngredient.createFromAbsoluteValues({
+        name: 'Raw Ingredient',
+        weight: 100,
+        protein: 30,
+        fat: 30,
+        carbohydrate: 30,
+        fiber: 10,
+        kcal: 400,
+      });
+
+      expect(rawIngredient).toBeInstanceOf(RawIngredient);
+      expect(rawIngredient.isRatioSumOne()).toBe(true);
+    });
+
+    it('should throw an error when protein is less than zero', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: -1,
+          fat: 30,
+          carbohydrate: 30,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Protein ratio cannot be less than 0');
+    });
+
+    it('should throw an error when fat is less than zero', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: -1,
+          carbohydrate: 30,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Fat ratio cannot be less than 0');
+    });
+
+    it('should throw an error when carbohydrate is less than zero', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: 30,
+          carbohydrate: -1,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Carbohydrate ratio cannot be less than 0');
+    });
+
+    it('should throw an error when fiber is less than zero', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: 30,
+          carbohydrate: 30,
+          fiber: -1,
+          kcal: 400,
+        }),
+      ).toThrowError('Fiber ratio cannot be less than 0');
+    });
+
+    it('should throw an error when protein is greater than weight', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 101,
+          fat: 30,
+          carbohydrate: 30,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Protein ratio cannot be greater than 1');
+    });
+
+    it('should throw an error when fat is greater than weight', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: 101,
+          carbohydrate: 30,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Fat ratio cannot be greater than 1');
+    });
+
+    it('should throw an error when carbohydrate is greater than weight', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: 30,
+          carbohydrate: 101,
+          fiber: 10,
+          kcal: 400,
+        }),
+      ).toThrowError('Carbohydrate ratio cannot be greater than 1');
+    });
+
+    it('should throw an error when fiber is greater than weight', () => {
+      expect(() =>
+        RawIngredient.createFromAbsoluteValues({
+          name: 'Raw Ingredient',
+          weight: 100,
+          protein: 30,
+          fat: 30,
+          carbohydrate: 30,
+          fiber: 101,
+          kcal: 400,
+        }),
+      ).toThrowError('Fiber ratio cannot be greater than 1');
+    });
+  });
 });
--- src/@core/domain/raw-ingredient/raw-ingredient.entity.ts
+++ src/@core/domain/raw-ingredient/raw-ingredient.entity.ts
@@ -12,6 +12,16 @@ export type CreateRawIngredientFromRatiosInput = NutritionalEntityProps & {
   name: string;
 };
 
+export type CreateFromAbsoluteValuesInput = {
+  name: string;
+  weight: number;
+  protein: number;
+  fat: number;
+  carbohydrate: number;
+  fiber: number;
+  kcal: number;
+};
+
 export class RawIngredient extends NutritionalEntity {
   private _id?: string;
   private _name: string;
@@ -26,6 +36,21 @@ export class RawIngredient extends NutritionalEntity {
     props: CreateRawIngredientFromRatiosInput,
   ): RawIngredient {
     const rawIngredient = new RawIngredient(props);
+    return rawIngredient;
+  }
+
+  public static createFromAbsoluteValues(
+    props: CreateFromAbsoluteValuesInput,
+  ): RawIngredient {
+    const rawIngredient = new RawIngredient({
+      name: props.name,
+      protein_ratio: props.protein / props.weight,
+      fat_ratio: props.fat / props.weight,
+      carbohydrate_ratio: props.carbohydrate / props.weight,
+      fiber_ratio: props.fiber / props.weight,
+      kcal_per_gram: props.kcal / props.weight,
+    });
+
     return rawIngredient;
   }
 
