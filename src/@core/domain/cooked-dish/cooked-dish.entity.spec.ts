1b9b2ea7f63f8c8d386600f78e396b3fe219a7d9
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-09-27 23:29:34 -0300

    Creating Cooked dish entity that is created based on amounts of raw ingredients and the resulting whole dish weight

---
+++ src/@core/domain/cooked-dish/cooked-dish.entity.spec.ts
@@ -0,0 +1,89 @@
+import { RawIngredient } from '../raw-ingredient/raw-ingredient.entity';
+import { CookedDish } from './cooked-dish.entity';
+
+describe('CookedDish', () => {
+  describe('createFromRawIngredients', () => {
+    it('should create a cooked dish from raw ingredients', () => {
+      const rawIngredients = [
+        {
+          raw_ingredient: RawIngredient.createFromRatios({
+            name: 'raw ingredient 1',
+            protein_ratio: 0.1,
+            fat_ratio: 0.2,
+            carbohydrate_ratio: 0.3,
+            fiber_ratio: 0.4,
+            kcal_per_gram: 0.5,
+          }),
+          amount_in_grams: 100,
+        },
+        {
+          raw_ingredient: RawIngredient.createFromRatios({
+            name: 'raw ingredient 2',
+            protein_ratio: 0.6,
+            fat_ratio: 0.7,
+            carbohydrate_ratio: 0.8,
+            fiber_ratio: 0.9,
+            kcal_per_gram: 1.0,
+          }),
+          amount_in_grams: 200,
+        },
+      ];
+
+      const cookedDish = CookedDish.createFromRawIngredients(
+        'cooked dish',
+        rawIngredients,
+        600,
+      );
+
+      expect(cookedDish.protein_ratio).toEqual(
+        rawIngredients.reduce(
+          (acc, rawIngredient) =>
+            acc +
+            rawIngredient.raw_ingredient.protein_ratio *
+              rawIngredient.amount_in_grams,
+          0,
+        ) / 600,
+      );
+
+      expect(cookedDish.fat_ratio).toEqual(
+        rawIngredients.reduce(
+          (acc, rawIngredient) =>
+            acc +
+            rawIngredient.raw_ingredient.fat_ratio *
+              rawIngredient.amount_in_grams,
+          0,
+        ) / 600,
+      );
+
+      expect(cookedDish.carbohydrate_ratio).toEqual(
+        rawIngredients.reduce(
+          (acc, rawIngredient) =>
+            acc +
+            rawIngredient.raw_ingredient.carbohydrate_ratio *
+              rawIngredient.amount_in_grams,
+          0,
+        ) / 600,
+      );
+
+      expect(cookedDish.fiber_ratio).toEqual(
+        rawIngredients.reduce(
+          (acc, rawIngredient) =>
+            acc +
+            rawIngredient.raw_ingredient.fiber_ratio *
+              rawIngredient.amount_in_grams,
+          0,
+        ) / 600,
+      );
+
+      expect(cookedDish.kcal_per_gram).toEqual(
+        rawIngredients.reduce(
+          (acc, rawIngredient) =>
+            acc +
+            rawIngredient.raw_ingredient.kcal_per_gram *
+              rawIngredient.amount_in_grams,
+          0,
+        ) / 600,
+      );
+    });
+  });
+});
---
+++ src/@core/domain/cooked-dish/cooked-dish.entity.ts
@@ -0,0 +1,72 @@
+import {
+  NutritionalEntity,
+  NutritionalEntityProps,
+} from '../nutritional-entity/nutritional-entity.entity';
+import { RawIngredient } from '../raw-ingredient/raw-ingredient.entity';
+
+export type RawIngredientAmount = {
+  raw_ingredient: RawIngredient;
+  amount_in_grams: number;
+};
+
+export type CookedDishProps = NutritionalEntityProps & {
+  id?: string;
+  name: string;
+};
+
+export class CookedDish extends NutritionalEntity {
+  private _id?: string;
+  private _name: string;
+
+  private constructor(props: CookedDishProps) {
+    super(props);
+    this._id = props.id;
+    this._name = props.name;
+  }
+
+  public static createFromRawIngredients(
+    name: string,
+    rawIngredients: RawIngredientAmount[],
+    finalWeightInGrams: number,
+  ): CookedDish {
+    let totalProtein = 0;
+    let totalFat = 0;
+    let totalCarbohydrate = 0;
+    let totalFiber = 0;
+    let totalKcal = 0;
+
+    rawIngredients.forEach((rawIngredient) => {
+      totalProtein +=
+        rawIngredient.raw_ingredient.protein_ratio *
+        rawIngredient.amount_in_grams;
+      totalFat +=
+        rawIngredient.raw_ingredient.fat_ratio * rawIngredient.amount_in_grams;
+      totalCarbohydrate +=
+        rawIngredient.raw_ingredient.carbohydrate_ratio *
+        rawIngredient.amount_in_grams;
+      totalFiber +=
+        rawIngredient.raw_ingredient.fiber_ratio *
+        rawIngredient.amount_in_grams;
+      totalKcal +=
+        rawIngredient.raw_ingredient.kcal_per_gram *
+        rawIngredient.amount_in_grams;
+    });
+
+    const protein_ratio = totalProtein / finalWeightInGrams;
+    const fat_ratio = totalFat / finalWeightInGrams;
+    const carbohydrate_ratio = totalCarbohydrate / finalWeightInGrams;
+    const fiber_ratio = totalFiber / finalWeightInGrams;
+    const kcal_per_gram = totalKcal / finalWeightInGrams;
+
+    const cookedDish = new CookedDish({
+      name,
+      protein_ratio,
+      fat_ratio,
+      carbohydrate_ratio,
+      fiber_ratio,
+      kcal_per_gram,
+    });
+
+    return cookedDish;
+  }
+}
