67c2c7c5ce73c46dcd5f1184cfe95ebb11675b89
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-09-29 09:44:15 -0300

    Using Fastify instead of Express

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
 
   app.enableCors();
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
