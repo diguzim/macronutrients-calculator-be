59bd7c36aa3d688b4e08cab5f867aebfd4a86137
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-10-12 17:46:17 -0300

    Moving config folder inside utils

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RawIngredientsModule } from './raw-ingredients/raw-ingredients.module';
-import configuration from './config/configuration';
+import environmentVariables from './utils/config/environment-variables';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
+      load: [environmentVariables],
     }),
     MongooseModule.forRoot('mongodb://localhost/nest'),
     RawIngredientsModule,
