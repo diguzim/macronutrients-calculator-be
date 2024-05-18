dc239ed74d1492f1a4ac12a66ea368f4ffef4e8c
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-05-18 18:21:07 -0300

    Creating typeorm standalone data source

---
+++ src/infra/database/typeorm/data-source.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+
+// This is a hack to allow data source to be used outside of the context of the Nest app
+if (!process.env.DB_HOST) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config();
+}
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  synchronize: false,
+  logging: true,
+  entities: [join(__dirname, '**/*.schema.ts')],
+  migrations: [join(__dirname, 'migrations', '*.ts')],
+  migrationsTableName: 'typeorm_migrations',
+  migrationsRun: false,
+});
