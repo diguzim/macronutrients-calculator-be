91eeb036d54f620b3db88e37da6b95e51a5fcb86
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-19 23:20:09 -0300

    Adjusting transport layer to item search by name

--- src/utils/serializers/item.serializer.ts
+++ src/utils/serializers/item.serializer.ts
@@ -2,6 +2,18 @@ import { Item } from '../../core/domain/item/item.entity';
 
 export class ItemSerializer {
   public static serialize(item: Item) {
-    return item;
+    return {
+      id: item.id,
+      name: item.name,
+      isPublic: item.isPublic,
+      type: item.type,
+      proteinRatio: item.proteinRatio,
+      fatRatio: item.fatRatio,
+      carbohydrateRatio: item.carbohydrateRatio,
+      fiberRatio: item.fiberRatio,
+      kcalPerGram: item.kcalPerGram,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt,
+    };
   }
 }
